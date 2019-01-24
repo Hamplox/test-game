74cd10ea6639482bcd5dd679887004792d4a65c7
Ninio <pereriksson98@hotmail.com>
2019-01-24 11:37:43 +0100

    hp stuff

--- Polk/Assets/PlayerMovement.cs
+++ Polk/Assets/PlayerMovement.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public int hp = 100;
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (false)
-        {
-          
-        }
+
         Debug.Log("Lol");
+        Debug.Log("why this no work ? ");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            hp--;
+        }
     }
 }
